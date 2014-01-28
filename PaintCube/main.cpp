7d55aac139d536f1e9422069ea14c4d529abae77
Paul Wright <paulwright@wl-10-190-251-15.wireless.duke.edu>
2014-01-28 17:16:11 -0500

    Initial Commit

---
+++ PaintCube/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PaintCube
+//
+//  Created by Paul Wright on 1/28/14.
+//  Copyright (c) 2014 Paul Wright. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
